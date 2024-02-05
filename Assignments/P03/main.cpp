c4dc39a30ca9fa0025741ed16ae8184f07c6a926
Austin <ausmor23@gmail.com>
2024-02-05 13:14:08 -0600

    finished Stack class

--- Assignments/P03/main.cpp
+++ Assignments/P03/main.cpp
@@ -4,17 +4,38 @@ class Stack{
 private:
   int* container;
   int size;
+  int offset;
 public:
   Stack(int s) {
     container = new int[size];
     size = s;
+    offset = 0;
   }
   Stack(int s, int* data) {
     container = data;
     size = s;
+    offset = size;
+  }
+
+  bool isFull() {
+    return offset == size;
+  }
+
+  bool isEmpty() {
+    return offset == 0;
+  }
+
+  void push(int x) {
+    container[offset++] = x;
+  }
+
+  int pop() {
+    if (!isEmpty())
+      return container[--offset];
   }
 };
-int main () {
+
+int main() {
   Stack pile(10);
   return 0;
 }
